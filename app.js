79cebd48afe44c66e7c7d9b9c067010a26a591a2
Sergey Topolov <sj_89@mail.ru>
2023-07-30 14:28:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express")
 
 const app = express()
 const port = process.env.PORT || 4000
+const service = process.env.SERVICE || "NODE"
 
 app.get("/", (req, res) => {
-  res.send(`Render on port ${port}`)
+  res.send(`${service} on port ${port}`)
 })
 
-app.listen(port, () => console.log(`on port ${port}`))
+app.listen(port, () => console.log(`${service} on port ${port}`))
 module.exports = app
