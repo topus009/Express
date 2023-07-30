6adc697f781f56ad6f47b8d68c27feb03787fcd0
Sergey Topolov <sj_89@mail.ru>
2023-07-30 14:26:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.get("/", (req, res) => {
-  res.send("Hello")
+  res.send(`Render on port ${port}`)
 })
 
 app.listen(port, () => console.log(`on port ${port}`))
